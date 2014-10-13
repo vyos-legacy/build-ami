7c65ace2bd9dac87dff879fbe6c8cc5face0d55e
Patrick van Staveren <trick@vanstaveren.us>
2014-10-13 02:25:00 +0000

    Fix syntax errors with variable expansion

--- nightly-build.sh
+++ nightly-build.sh
@@ -4,11 +4,11 @@ set -u
 set -e
 
 nightly_iso_dir=http://dev.packages.vyos.net/iso/helium/amd64/
-nightly_iso_dir_formatted="{$nightly_iso_dir}?F=0"
+nightly_iso_dir_formatted="${nightly_iso_dir}?F=0"
 
 recent_iso=$(curl -s $nightly_iso_dir_formatted | grep "^<li>" | cut -d\" -f2 | tail -n 1)
 
-recent_iso_url="{$nightly_iso_dir}{$recent_iso}"
+recent_iso_url="${nightly_iso_dir}${recent_iso}"
 
 matching_images=$(aws ec2 describe-images --filters "Name=tag:iso,Values=$recent_iso" | wc -l)
 if [ $matching_images -eq 0 ]; then
