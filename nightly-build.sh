e600c17f075f2f817cf199fe861e15b958253b21
Patrick van Staveren <trick@vanstaveren.us>
2016-02-19 21:46:49 +0000

    allow to build a nightly based on a specific branch

--- nightly-build.sh
+++ nightly-build.sh
@@ -11,12 +11,19 @@ if [ -d env ]; then
     set -u
 fi
 
-nightly_iso_dir=http://dev.packages.vyos.net/iso/helium/amd64/
+if [ $# -eq 1 ]; then
+    branch=$1
+else
+    branch=helium
+fi
+
+nightly_iso_dir=http://dev.packages.vyos.net/iso/$branch/amd64/
 nightly_iso_dir_formatted="${nightly_iso_dir}?F=0"
 
 recent_iso=$(curl -s $nightly_iso_dir_formatted | grep "^<li>" | cut -d\" -f2 | tail -n 1)
 
 recent_iso_url="${nightly_iso_dir}${recent_iso}"
+echo "ISO: $recent_iso_url"
 
 matching_images=$(aws ec2 describe-images --filters "Name=tag:iso,Values=$recent_iso" | wc -l)
 if [ $matching_images -eq 0 ]; then
